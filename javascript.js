f6bb4061b7732e8003db84fc430770ce86a0b972
Dominic.W.Kantrude <dominic.kantrude52@yahoo.com>
2019-02-06 14:02:39 -0600

    added keyup functionality

--- javascript.js
+++ javascript.js
@@ -1,20 +1,39 @@
-document.querySelector("#submit").addEventListener("click", function(){
-   let height= document.querySelector("#height").value
-    let char = document.querySelector("#char").value
-    if(char.length === 1){
-  
-    printChristmasTree(height, char)
-    }else{
-        console.log("Must be one character")
+document.querySelector("#submit").addEventListener("click", function () {
+    buildChristmasObject()
+})
+
+document.querySelector("#char").addEventListener("keyup", function () {
+    console.log(event.keyCode)
+     if (event.keyCode === 13) {
+         buildChristmasObject()
+     }
+ })
+
+document.querySelector("#height").addEventListener("keyup", function () {
+   console.log(event.keyCode)
+    if (event.keyCode === 13) {
+        buildChristmasObject()
     }
 })
 
+const buildChristmasObject = () => {
+    let char = document.querySelector("#char").value
+    if (char.length === 1) {
+        let tree = {
+            height: document.querySelector("#height").value,
+            char: char
+        }
+        printChristmasTree(tree)
+    } else {
+        console.log("Must be one character")
+    }
+}
 
-const printChristmasTree = (height, letter) => {
+const printChristmasTree = (tree) => {
     let space = " "
     let letterCounter = 1;
-        for(let i=height; i != 0; i--){
-            console.log(space.repeat(i)+ letter.repeat(letterCounter))
-            letterCounter+=2;
-        }
+    for (let i = tree.height; i != 0; i--) {
+        console.log(space.repeat(i) + tree.char.repeat(letterCounter))
+        letterCounter += 2;
+    }
 }   
